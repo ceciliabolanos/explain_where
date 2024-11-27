9fd99ac83b0b51df3853d592fd2c3891c1685402
ceciliabolanos <chechubolanos@gmail.com>
2024-11-27 18:19:19 +0000

    dataframe with father ids

--- preprocess/dataframe.py
+++ preprocess/dataframe.py
@@ -1,9 +1,11 @@
-"""Process the csv and also get the labels from level 0, 1 and 2"""
+"""Process the csv and also get the labels from level 0, 1 and 2. If a label is from level 2,
+    we also get the father label. We also calculate the duration of each label. We save the results in a new csv file. """
 
 import pandas as pd
 import json 
 from utils import get_name_id_mappings
 import numpy as np
+from transformers import AutoFeatureExtractor, ASTForAudioClassification
 
 def get_label_name(label_id, ontology_data):
     for entry in ontology_data:
@@ -16,7 +18,10 @@ def get_father_label_name(label_id, mappings):
 ############# Preprocess to get the audio files we are going to run and the labels we have segmented for each one. #############
 
 df_segmented = pd.read_csv('/home/cbolanos/experiments/audioset/labels/audioset_train_strong.tsv', sep='\t')
-df_segmented['base_segment_id'] = df_segmented['segment_id'].apply(lambda x: x.rsplit('_', 1)[0])    
+df_segmented['base_segment_id'] = df_segmented['segment_id'].apply(lambda x: x.rsplit('_', 1)[0])
+
+feature_extractor = AutoFeatureExtractor.from_pretrained("MIT/ast-finetuned-audioset-10-10-0.4593")
+model = ASTForAudioClassification.from_pretrained("MIT/ast-finetuned-audioset-10-10-0.4593")
 
 with open('/home/cbolanos/experiments/audioset/labels/ontology.json', 'r') as file:
      ontology_data = json.load(file)
@@ -30,7 +35,10 @@ mappings = get_name_id_mappings(ontology_data)
 df_segmented['father_labels'] = df_segmented['label'].apply(
         lambda x: get_father_label_name(x, mappings)
     )
-
+df_segmented['father_labels_ids'] = df_segmented['father_labels'].apply(
+    lambda x: model.config.label2id.get(x, -1)
+).astype('Int64')
+df_segmented = df_segmented[df_segmented['father_labels_ids'] != -1]
 df_segmented = df_segmented[df_segmented['father_labels'].notna()]
 df_segmented['label_duration'] = (df_segmented['end_time_seconds'] - df_segmented['start_time_seconds']) * 1000
 df_segmented.to_csv('/home/cbolanos/experiments/audioset/labels/labels_segments.csv')
