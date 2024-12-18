1a373ed66fb313c195d3e508e57f584cc55a5ff3
ceciliabolanos <chechubolanos@gmail.com>
2024-12-18 00:27:54 +0000

    add every label to reach the firsts levels

--- preprocess/dataframe.py
+++ preprocess/dataframe.py
@@ -6,6 +6,9 @@ import json
 from utils import get_name_id_mappings
 import numpy as np
 from transformers import ASTForAudioClassification
+import networkx as nx
+from typing import List, Dict, Tuple, Set
+
 
 def get_label_name(label_id, ontology_data):
     for entry in ontology_data:
@@ -15,6 +18,37 @@ def get_label_name(label_id, ontology_data):
 def get_father_label_name(label_id, mappings):
     return mappings.get(label_id, [np.nan, np.nan, np.nan])[2]
 
+
+def get_all_ancestors(label_id: str, mappings: Dict[str, Tuple[int, str, str]], 
+                     ontology_data: List[dict]) -> Tuple[List[str], List[int]]:
+    """
+    Get all ancestor labels and their corresponding IDs for a given label ID
+    
+    Args:
+        label_id (str): The ID of the label
+        mappings (dict): The hierarchy mappings from get_name_id_mappings
+        ontology_data (list): The original ontology data
+    
+    Returns:
+        tuple: (List of ancestor names, List of ancestor IDs)
+    """
+    # Create graph from ontology
+    G = nx.DiGraph()
+    for item in ontology_data:
+        if 'child_ids' in item:
+            for child_id in item['child_ids']:
+                G.add_edge(item['id'], child_id)
+    
+    try:
+        # Get all ancestors using networkx
+        ancestor_ids = list(nx.ancestors(G, label_id))
+        # Get names and model IDs for all ancestors
+        ancestor_names = [get_label_name(ancestor_id, ontology_data) for ancestor_id in ancestor_ids]
+        ancestor_model_ids = [model.config.label2id.get(name, -1) for name in ancestor_names if name is not None]
+        return ancestor_names, ancestor_model_ids
+    except:
+        return [], []
+    
 ############# Preprocess to get the audio files we are going to run and the labels we have segmented for each one. #############
 
 df_segmented = pd.read_csv('/home/cbolanos/experiments/audioset/labels/audioset_eval_strong.tsv', sep='\t')
@@ -30,6 +64,12 @@ df_segmented['positive_labels'] = df_segmented['label'].apply(
     )
 
 mappings = get_name_id_mappings(ontology_data)
+ancestor_results = df_segmented['label'].apply(
+    lambda x: get_all_ancestors(x, mappings, ontology_data)
+)
+
+df_segmented['other_labels'] = ancestor_results.apply(lambda x: x[0])
+df_segmented['other_labels_ids'] = ancestor_results.apply(lambda x: x[1])
 
 df_segmented['father_labels'] = df_segmented['label'].apply(
         lambda x: get_father_label_name(x, mappings)
