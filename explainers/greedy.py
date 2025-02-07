dc08343d6f35ec4dcf24129b324cdd2a0279ba23
ceciliabolanos <chechubolanos@gmail.com>
2025-02-07 11:56:37 +0000

    adding greedy

--- explainers/greedy.py
+++ explainers/greedy.py
@@ -12,11 +12,12 @@ class GreedyExplainer:
         
         indexes = data['scores']
         N = len(indexes)
-        importance_vector = np.zeros(max(indexes) + 1)
+        importance_vector = np.zeros(N)
 
         importance_values = [1 - (i / (N - 1)) for i in range(N)]
 
         for idx, importance in zip(indexes, importance_values):
+            idx = int(idx)
             importance_vector[idx] = importance
 
         return importance_vector
