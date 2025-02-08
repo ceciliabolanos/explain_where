c0d4dcf744b39ff7be89ec4fa9b5426b50a877cd
ceciliabolanos <chechubolanos@gmail.com>
2025-02-08 00:21:18 +0000

    cough exp

---
+++ experiments_cough.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from tqdm import tqdm
+from run_explanation import generate_explanation
+from data.base_generator import MaskingConfig
+import os 
+
+LABELS_PATH = '/home/cbolanos/explain_where/models/cough/cough_happy.csv'
+SEGMENT_LENGTH = 100
+NUM_SAMPLES = 3000
+
+mask_percentages = [0.2, 0.3, 0.4]
+window_sizes = [1, 3, 5]
+mask_types = ['zeros', 'stat', 'noise']
+
+df = pd.read_csv(LABELS_PATH)
+
+for mask_type in mask_types:
+    for window_size in window_sizes: 
+        for mask_percentage in mask_percentages:
+            mask_config = MaskingConfig(segment_length=SEGMENT_LENGTH, 
+                                        mask_percentage=mask_percentage, 
+                                        window_size=window_size,
+                                        num_samples=NUM_SAMPLES, 
+                                        function='euclidean',
+                                        mask_type=mask_type)
+            j =0
+            for i in tqdm(range(len(df))):
+                filename = df.loc[i, 'filename']
+                if j < 50:
+                    generate_explanation(
+                        filename=filename,
+                        model_name='cough',
+                        id_to_explain=1,
+                        config=mask_config,
+                        path='/home/ec2-user/results1/explanations_cough'
+                    )
+                j=j+1
