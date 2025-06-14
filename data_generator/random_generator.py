e49882e2c254644cf7fd6298462e511c78f5869d
chechu <chechubolanos@gmail.com>
2025-06-14 11:30:26 +0000

    adding a path

--- data_generator/random_generator.py
+++ data_generator/random_generator.py
@@ -50,7 +50,7 @@ class RandomDataGenerator:
         snrs = []
         chosen_file = ( # use the last combination to get the info of the normal input
                 Path(self.path) / filename / self.model_name /
-                f"scores_p{mask_percentage}_w{window_size}_f{function}_m{mask_type}_a{std_noise}.json"
+                f"scores_p{mask_percentage}_w{window_size}_f{function}_m{mask_type}.json"
             )
         with open(chosen_file, "r") as f:
             data = json.load(f)
